0dbfb18e2bf9289796a2d55e9f6f3afc296c0eaf
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2023-12-25 16:21:45 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -28,9 +28,9 @@ GRUB=/etc/default
 MODPROBE=/etc/modprobe.d
 SYSCTL=/etc/sysctl.d
 
-if [ "$EUID" -ne 0 ]
-  then echo "[+] Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+	echo "[+] Please run as root"
+	exit
 fi
 
 echo "WARNING Be careful: Backup your data, Create snapshot and more"
@@ -38,30 +38,33 @@ read -rp "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][
 
 echo "[+] Backup sysctl files"
 for file in /etc/sysctl.d/*; do
-    if [ -f "$file" ]; then
-        cp "$file" "$file".bk
-    fi
-done
-
-echo "[+] Backup modprobe files"
-for file in /etc/modprobe.d/*; do
-    if [ -f "$file" ]; then
-        cp "$file" "$file".bk
-    fi
+	if [ -f "$file" ]; then
+		cp "$file" "$file".bk
+	fi
 done
 
 echo "[+] Copy sysctl.conf to $SYSCTL"
 cp ./sysctl.conf "$SYSCTL"/sysctl.conf
 
+echo "[+] Backup modprobe files"
+for file in /etc/modprobe.d/*; do
+	if [ -f "$file" ]; then
+		cp "$file" "$file".bk
+	fi
+done
+
 echo "[+] Copy blacklist.conf to $MODPROBE"
 cp ./blacklist.conf "$MODPROBE"/blacklist.conf
 
 if [ -f "$GRUB"/grub ]; then
-    echo "[+] Backup $GRUB/grub file"
-    cp "$GRUB"/grub "$GRUB"/grub.bk
-    echo "[+] Copy GRUB_CMDLINE_LINUX_DEFAULT value into $GRUB/grub"
-    sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/d' "$GRUB"/grub
-    echo "GRUB_CMDLINE_LINUX_DEFAULT=\"l1tf=full,force page_poison=on pti=on slab_nomerge=yes slub_debug=FZP spec_store_bypass_disable=seccomp spectre_v2=on mds=full,nosmt mce=0 page_alloc.shuffle=1 rng_core.default_quality=500 init_on_alloc=1 init_on_free=1 randomize_kstack_offset=on vsyscall=none debugfs=off oops=panic module.sig_enforce=1 lockdown=confidentiality quiet loglevel=0 tsx=off tsx_async_abort=full,nosmt nosmt=force kvm.nx_huge_pages=force\"" >> "$GRUB"/grub
+	echo "[+] Backup $GRUB/grub file"
+	cp "$GRUB"/grub "$GRUB"/grub.bk
+	echo "[+] Copy GRUB_CMDLINE_LINUX_DEFAULT value into $GRUB/grub"
+	sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/d' "$GRUB"/grub
+	sed -i '/GRUB_CMDLINE_LINUX/d' "$GRUB"/grub
+	echo "" >>"$GRUB"/grub
+	echo "#KernelHardened configuration"
+	echo "GRUB_CMDLINE_LINUX=\"l1tf=full,force page_poison=on pti=on slab_nomerge=yes slub_debug=FZP spec_store_bypass_disable=seccomp spectre_v2=on mds=full,nosmt mce=0 page_alloc.shuffle=1 rng_core.default_quality=500 init_on_alloc=1 init_on_free=1 randomize_kstack_offset=on vsyscall=none debugfs=off oops=panic module.sig_enforce=1 lockdown=confidentiality quiet loglevel=0 tsx=off tsx_async_abort=full,nosmt nosmt=force kvm.nx_huge_pages=force\"" >>"$GRUB"/grub
 else
-    echo "[-] Error, grub default file not found"
+	echo "[-] Error, grub default file not found"
 fi
